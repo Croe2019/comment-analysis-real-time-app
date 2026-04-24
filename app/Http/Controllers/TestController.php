c1c8c3f9d91fac7b53870e11b8ed39b31891fd11
Croe2019 <youremail@example.com>
2026-04-24 14:35:56 +0900

    URL修正

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -9,7 +9,7 @@ class TestController extends Controller
 {
     public function youtube()
     {
-        $url = 'https://www.youtube.com/watch?v=W0hpBFmA2t4'; // ライブ配信URLにする
+        $url = 'https://www.youtube.com/watch?v='; // ライブ配信URLにする
 
         $youtubeService = new YoutubeService();
 
